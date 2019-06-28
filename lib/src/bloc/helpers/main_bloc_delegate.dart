c09448aa1917c460c1e2c1e79650ad5d258240f6
asyrafsalim <ninjatrickz@gmail.com>
2019-06-28 16:58:34 +0800

    setup bloc

--- lib/src/bloc/helpers/main_bloc_delegate.dart
+++ lib/src/bloc/helpers/main_bloc_delegate.dart
@@ -7,18 +7,18 @@ class MainBlocDelegate extends BlocDelegate {
   @override
   void onEvent(Bloc bloc, Object event) {
     super.onEvent(bloc, event);
-    print(event);
+    print("BlocDelegate event: $event");
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    print(transition);
+    print("BlocDelegate transition: $transition");
   }
 
   @override
   void onError(Bloc bloc, Object error, StackTrace stacktrace) {
     super.onError(bloc, error, stacktrace);
-    print(error);
+    print("BlocDelegate error: $error");
   }
 }
