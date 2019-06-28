1aadd9d84e4dbb0f645ddfbed534a6f66d1c80e0
asyrafsalim <ninjatrickz@gmail.com>
2019-06-28 16:59:13 +0800

    load blocs to widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import './src/bloc/helpers/main_bloc_delegate.dart';
+import './src/bloc/blocs/todo/bloc.dart';
 import './src/app.dart';
 
 void main() {
   BlocSupervisor.delegate = MainBlocDelegate();
-  runApp(App());
+  runApp(
+    BlocProviderTree(
+      blocProviders: [
+        BlocProvider<TodoBloc>(
+          builder: (context) => TodoBloc(),
+        ),
+      ],
+      child: App(),
+    ),
+  );
 }
